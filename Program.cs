98330b6bd1bcd97909e702779922e7e2841b0f35
JL-J <40110348+JL-J@users.noreply.github.com>
2019-01-16 17:02:05 +0000

    User enters a number

--- Program.cs
+++ Program.cs
@@ -12,10 +12,27 @@ namespace NumberGuesser
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"{appName}: Version {appVersion} by {appAuthor}");
-            Console.ResetColor(); 
+            Console.ResetColor();
 
+            Console.WriteLine("What is your name?");
+            string usersName = Console.ReadLine();
 
-            
+            Console.WriteLine($"Ok {usersName}, let's play a game!");
+
+            int randomNumber = 7;
+            int guess = 0;
+            Console.WriteLine("Guess a number between 1 and 10.");
+            while (guess != randomNumber)
+            {
+                string usersGuess = Console.ReadLine();
+                guess = Int32.Parse(usersGuess); 
+                if (guess != randomNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Sorry, that's not the number I'm thinking of. Please guess again.");
+                    Console.ResetColor();
+                }
+            }
         }
     }
 }
