c50f2fec22ba426030c39072cdc8dc88683221fb
JL-J <40110348+JL-J@users.noreply.github.com>
2019-01-16 18:00:24 +0000

    Refactor code

--- Program.cs
+++ Program.cs
@@ -6,18 +6,9 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            string appName = "Number Guesser";
-            string appVersion = "1.0.0";
-            string appAuthor = "JLJ";
+            AppHeader();
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{appName}: Version {appVersion} by {appAuthor}");
-            Console.ResetColor();
-
-            Console.WriteLine("What is your name?");
-            string usersName = Console.ReadLine();
-
-            Console.WriteLine($"Ok {usersName}, let's play a game!");
+            GreetUser();
 
             while (true)
             {
@@ -32,22 +23,16 @@ namespace NumberGuesser
                     string usersGuess = Console.ReadLine();
                     if (!int.TryParse(usersGuess, out guess))
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Sorry, but that's not a number. Please try again");
-                        Console.ResetColor();
+                        PrintMessage("Sorry, but that's not a number. Please try again.", ConsoleColor.Red); 
                         continue;
                     }
                     guess = Int32.Parse(usersGuess);
                     if (guess != randomNumber)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Sorry, that's not the number I'm thinking of. Please guess again.");
-                        Console.ResetColor();
+                        PrintMessage("Sorry, that's not the number I'm thinking of. Please guess again.", ConsoleColor.Red);
                     }
                 }
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Congrats! You guessed my number!");
-                Console.ResetColor();
+                PrintMessage("Congrats! You guessed my number!", ConsoleColor.Yellow);
 
                 Console.WriteLine("Do you want to play again? [Y or N]");
                 string answer = Console.ReadLine().ToUpper();
@@ -59,8 +44,33 @@ namespace NumberGuesser
                 {
                     return;
                 }
-                     
             }
+        }
+
+        static void AppHeader()
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "JLJ";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{appName}: Version {appVersion} by {appAuthor}");
+            Console.ResetColor();
+        }
+
+        static void GreetUser()
+        {
+            Console.WriteLine("What is your name?");
+            string usersName = Console.ReadLine();
+
+            Console.WriteLine($"Ok {usersName}, let's play a game!");
+        }
+
+        static void PrintMessage(string message, ConsoleColor colour)
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
